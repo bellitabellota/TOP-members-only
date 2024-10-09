d69a6579745c97fa636f638f4a6827e782f6de55
bellitabellota <136174454+bellitabellota@users.noreply.github.com>
2024-10-09 16:26:05 +0100

    Create associated Post model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_10_09_124246) do
+ActiveRecord::Schema[7.2].define(version: 2024_10_09_152422) do
+  create_table "posts", force: :cascade do |t|
+    t.string "title"
+    t.text "body"
+    t.integer "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_posts_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "name", null: false
     t.string "email", default: "", null: false
@@ -23,4 +32,6 @@ ActiveRecord::Schema[7.2].define(version: 2024_10_09_124246) do
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
+
+  add_foreign_key "posts", "users"
 end
